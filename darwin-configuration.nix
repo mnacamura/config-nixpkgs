54fc9bb788c05a338e7b2d08a82fc896ef0126e5
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2017-07-12 01:27:10 +0900

    Add Nix Darwin configuration

---
+++ darwin-configuration.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+{
+  nix.nixPath = [
+    "nixpkgs=$HOME/.nix-defexpr/channels/nixpkgs"
+    "darwin=$HOME/.nix-defexpr/channels/darwin"
+    "darwin-config=$HOME/.config/nixpkgs/darwin-configuration.nix"
+    "$HOME/.nix-defexpr/channels"
+  ];
+  nix.maxJobs = 4;
+  nix.buildCores = 0;
+  nix.useSandbox = false;
+
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+  ];
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.fish.enable = true;
+
+  services.activate-system.enable = true;
+  services.nix-daemon.enable = true;
+}
