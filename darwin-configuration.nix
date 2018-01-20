6ef87277161f269adc68ce8f6a37ce71d1fb9135
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-01-20 12:52:46 +0900

    Refactor darwin config

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,34 +1,26 @@
 { config, lib, pkgs, ... }:
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     nix-repl
   ];
   environment.variables.EDITOR = "vim";  # Available by default on macOS
 
-  # Create /etc/fish that loads the nix-darwin environment.
   programs.fish.enable = true;
   environment.shells = with pkgs; [ fish ];
 
-  # Recreate /run/current-system symlink after boot.
-  services.activate-system.enable = true;
   services.nix-daemon.enable = true;
+
+  nix.trustedUsers = [ "@admin" ];
+  # nix.useSandbox = true;  # still does not work 2018-01-20
+  nix.package = pkgs.nix;
+  nix.maxJobs = 4;
+  nix.nixPath = [
+    "darwin-config=$HOME/.config/nixpkgs/darwin-configuration.nix"
+    "darwin=$HOME/.nix-defexpr/channels/darwin"
+    "nixpkgs=$HOME/.nix-defexpr/channels/nixpkgs"
+  ];
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 2;
-
-  # You should generally set this to the total number of logical cores in your system.
-  # $ sysctl -n hw.ncpu
-  nix.maxJobs = 4;
-
-  # Customize $NIX_PATH
-  nix.nixPath = [
-    "nixpkgs=$HOME/.nix-defexpr/channels/nixpkgs"
-    "darwin=$HOME/.nix-defexpr/channels/darwin"
-    "darwin-config=$HOME/.config/nixpkgs/darwin-configuration.nix"
-    "/nix/var/nix/profiles/per-user/root/channels"
-    "$HOME/.nix-defexpr/channels"
-  ];
 }
