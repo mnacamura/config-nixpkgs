52dda2c9c9621924bcef1091420c5c9446370a42
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-02-14 16:46:34 +0900

    jupyterEnv: Add vim-binding

--- pkgs/jupyter/default.nix
+++ pkgs/jupyter/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, makeWrapper, pkgconfig, libxml2, libxslt, libpng, freetype,
+{ stdenv, makeWrapper, cacert, pkgconfig, libxml2, libxslt, libpng, freetype,
 python36Packages, python36 }:
 
 stdenv.mkDerivation rec {
@@ -7,17 +7,20 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     makeWrapper
+    cacert
     pkgconfig
+    python36Packages.pip
   ];
 
   buildInputs = [
     libxml2 libxslt  # dependencies for lxml
     libpng freetype  # dependencies for matplotlib
-  ] ++ (with python36Packages; [ python36 pip virtualenv ]);
+  ] ++ (with python36Packages; [ python36 virtualenv ]);
 
   phases = [ "installPhase" ];
 
   installPhase = ''
+    export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
     venv=$out/var/venvs/jupyter
     pip="pip --cache-dir=/var/cache/wheel"
     mkdir -p $venv
@@ -28,6 +31,9 @@ stdenv.mkDerivation rec {
     $pip install jupyter jupyter_contrib_nbextensions
     jupyter contrib nbextension install --sys-prefix
     $pip install jupyterthemes
+    jupyter nbextension install \
+        https://raw.githubusercontent.com/lambdalisue/jupyter-vim-binding/master/vim_binding.js \
+        --nbextensions=$venv/share/jupyter/nbextensions/vim_binding
     for bin in $venv/bin/{jupyter,jt}; do
         makeWrapper $bin $out/bin/$(basename $bin) \
             --run "source $venv/bin/activate"
