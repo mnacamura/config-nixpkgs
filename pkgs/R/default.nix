0242942343d0f5901017df6c790e2aa4261628b1
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-04-21 12:01:31 +0900

    rEnv: Refactor implementation

--- pkgs/R/default.nix
+++ pkgs/R/default.nix
@@ -1,58 +1,55 @@
-{ stdenv, buildEnv, R, rWrapper, rPackages }:
+{ R, rWrapper, rPackages }:
 
-let
-  myR = rWrapper.override {
-    packages = with rPackages; [
-      GGally
-      JuniperKernel
-      doParallel
-      dplyr
-      forcats
-      foreach
-      ggplot2
-      glue
-      hms
-      jsonlite
-      lubridate
-      magrittr
-      purrr
-      readr
-      readxl
-      rlang
-      rstan
-      stringr
-      tibble
-      tidyr
-    ];
-  };
-
-  version = stdenv.lib.getVersion R;
-in
-
-buildEnv rec {
-  name = "R-${version}-env";
-
-  ignoreCollisions = true;
-
-  pathsToLink = [ "/bin" "/lib" "/share" ];
-
-  paths = [
-    (stdenv.lib.lowPrio R)  # installs man pages etc.
-    myR
+(rWrapper.override ({
+  packages = with rPackages; [
+    GGally
+    JuniperKernel
+    doParallel
+    dplyr
+    forcats
+    foreach
+    ggplot2
+    glue
+    hms
+    jsonlite
+    lubridate
+    magrittr
+    purrr
+    readr
+    readxl
+    rlang
+    rstan
+    stringr
+    tibble
+    tidyr
   ];
+})).overrideAttrs (old: {
+  name = "${R.name}-env";
+
+  installPhase = old.installPhase + ''
+    # Install info and man pages
+    for path in `find ${R}/share`; do
+      dest=$out/''${path#${R}}
+      if [ -d $path ]; then
+        [ -d $dest ] || mkdir -p $dest
+      else
+        ln -s $path $dest
+      fi
+    done
+  '';
 
   postBuild = ''
     # Install JuniperKernerl
-    kspec=$out/share/jupyter/kernels/juniper-r-${version}
+    kspec=$out/share/jupyter/kernels/juniper-${R.name}
     mkdir -p $kspec
     cat << JSON > $kspec/kernel.json
     {
-      "argv": ["$out/bin/R", "--slave", "-e", "JuniperKernel::bootKernel()", "--args", "{connection_file}"],
-      "display_name": "${name} (Juniper)",
-      "language": "R"
+    "argv": ["$out/bin/R", "--slave", "-e", "JuniperKernel::bootKernel()", "--args", "{connection_file}"],
+    "display_name": "${R.name} (Juniper)",
+    "language": "R"
     }
     JSON
     chmod 444 $kspec/kernel.json
     ln -s ${rPackages.JuniperKernel}/library/JuniperKernel/extdata/logo-64x64.png $kspec/
   '';
-}
+})
