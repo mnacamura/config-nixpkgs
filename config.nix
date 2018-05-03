a955ab58af802e64712000482e7d259fcb72cc9e
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-05-04 00:05:19 +0900

    Styling

--- config.nix
+++ config.nix
@@ -4,6 +4,38 @@
 
   packageOverrides = super: let self = super.pkgs; in {
 
+    ## Custom packages
+
+    ccacheWrapper = super.ccacheWrapper.override {
+      extraConfig = ''
+        export CCACHE_COMPRESS=1
+        export CCACHE_DIR=/var/cache/ccache
+        export CCACHE_UMASK=007
+      '';
+    };
+
+    neovim = with super; neovim.override {
+      withRuby = false;
+      configure = {
+        customRC = ''
+          let $MYVIMRC = $HOME . '/.config/nvim/init.vim'
+          if filereadable($MYVIMRC)
+            source $MYVIMRC
+          else
+            echomsg 'Warning: ' . $MYVIMRC . ' is not readable'
+          endif
+        '';
+        packages.default = with vimPlugins; {
+          start = [ skim ];
+          opt = [];
+        };
+      };
+    };
+
+    mathjax = super.callPackage ./pkgs/mathjax {};
+
+    ## Environments
+
     adminEnv = with self; buildEnv {
       name = "admin-env";
       paths = [
@@ -62,32 +94,6 @@
       ];
     });
 
-    ccacheWrapper = with super; ccacheWrapper.override {
-      extraConfig = ''
-        export CCACHE_COMPRESS=1
-        export CCACHE_DIR=/var/cache/ccache
-        export CCACHE_UMASK=007
-      '';
-    };
-
-    neovim = with super; neovim.override {
-      withRuby = false;
-      configure = {
-        customRC = ''
-          let $MYVIMRC = $HOME . '/.config/nvim/init.vim'
-          if filereadable($MYVIMRC)
-            source $MYVIMRC
-          else
-            echomsg 'Warning: ' . $MYVIMRC . ' is not readable'
-          endif
-        '';
-        packages.default = with vimPlugins; {
-          start = [ skim ];
-          opt = [];
-        };
-      };
-    };
-
     desktopEnv = with self; buildEnv {
       name = "desktop-env";
       ignoreCollisions = true;
@@ -105,24 +111,24 @@
       ];
     };
 
-    mathematica = super.mathematica.override { lang = "ja"; };
-
-    texliveEnv = with self; let myTexlive = texlive.combine {
-      inherit (texlive)
-      scheme-basic  # installs collection-{basic,latex}
-      collection-luatex
-      collection-latexrecommended
-      collection-latexextra
-      collection-bibtexextra
-      collection-fontsrecommended
-      collection-fontsextra
-      collection-fontutils
-      collection-langjapanese
-      latexmk
-      latexdiff
-      revtex;
-    };
-    version = lib.getVersion myTexlive; in buildEnv {
+    texliveEnv = with self; let
+      myTexlive = texlive.combine {
+        inherit (texlive)
+        scheme-basic  # installs collection-{basic,latex}
+        collection-luatex
+        collection-latexrecommended
+        collection-latexextra
+        collection-bibtexextra
+        collection-fontsrecommended
+        collection-fontsextra
+        collection-fontutils
+        collection-langjapanese
+        latexmk
+        latexdiff
+        revtex;
+      };
+      version = lib.getVersion myTexlive;
+    in buildEnv {
       name = "texlive-${version}-env";
       paths = [
         ghostscript  # required by LaTeXiT
@@ -145,13 +151,11 @@
       '';
     };
 
-    jupyterEnv = with self; callPackage ./pkgs/jupyter {};
+    jupyterEnv = with self; callPackage ./pkgs/jupyter/env.nix {};
 
-    mathjax = with super; callPackage ./pkgs/mathjax {};
+    rEnv = self.callPackage ./pkgs/R/env.nix {};
 
-    rEnv = with self; callPackage ./pkgs/R/env.nix {};
-
-    juliaEnv = with self; callPackage ./pkgs/julia {};
+    juliaEnv = with self; callPackage ./pkgs/julia/env.nix {};
 
     rustEnv = with self;
     let version = rustc.version; in buildEnv {
