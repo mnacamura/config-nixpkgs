82f322ada93096ec198570df299d43dae18c4a53
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-08-07 23:23:15 +0900

    Add direnv config

--- overlays/default-overlay.nix
+++ overlays/default-overlay.nix
@@ -101,92 +101,101 @@ self: super:
 
   fishExtraConfig = with self; let
     name = "fish-extra-config";
-    git = writeText "${name}-git" ''
-      set -q __fish_config_git_sourced
-      or if status is-interactive
-        set __fish_git_prompt_showdirtystate y
-        set __fish_git_prompt_showcolorhints y
-        set __fish_git_prompt_showstashstate y
-        set __fish_git_prompt_showuntrackedfiles y
-        set __fish_git_prompt_showupstream auto
-        set __fish_git_prompt_char_untrackedfiles '?'
-        set __fish_git_prompt_char_upstream_equal '''
-        set __fish_git_prompt_char_upstream_behind '↓'
-        set __fish_git_prompt_char_upstream_ahead '↑'
-        set __fish_git_prompt_char_upstream_diverged '↓↑'
-        set __fish_git_prompt_color_untrackedfiles $fish_status_color_warn
-        abbr --add g   'git'
-        abbr --add ga  'git add'
-        abbr --add gb  'git branch'
-        abbr --add gc  'git commit'
-        abbr --add gcl 'git clone'
-        abbr --add gco 'git checkout'
-        abbr --add gd  'git diff'
-        abbr --add gdc 'git diff --cached'
-        abbr --add gdd 'git diff --no-ext-diff'
-        abbr --add gl  'git log'
-        abbr --add glg 'git log --graph --oneline'
-        abbr --add gp  'git pull'
-        abbr --add gs  'git status'
-        abbr --add gss 'git show --ext-diff'
-      end
-      set -g __fish_config_git_sourced 1
-    '';
-    less = writeText "${name}-less" ''
-      set -q __fish_config_less_sourced
-      or if status is-login
-        # Set to fit Srcery color scheme
-        set -Ux LESS_TERMCAP_mb (printf "\e[1m")                    # Begin blinking
-        set -Ux LESS_TERMCAP_md (printf "\e[1;31m")                 # Begin bold
-        set -Ux LESS_TERMCAP_me (printf "\e[0m")                    # End mode
-        set -Ux LESS_TERMCAP_so (printf "\e[1;30;48;2;214;93;14m")  # Begin standout mode
-        set -Ux LESS_TERMCAP_se (printf "\e[0m")                    # End standout mode
-        set -Ux LESS_TERMCAP_us (printf "\e[3;33m")                 # Begin underline
-        set -Ux LESS_TERMCAP_ue (printf "\e[0m")                    # End underline
-      end
-      if status is-interactive
-        set -gx LESS '-R -ig -j.5'
-      end
-      set -g __fish_config_less_sourced 1
-    '';
-    lsColors = writeText "${name}-ls-colors" ''
-      set -q __fish_config_ls_colors_sourced
-      or if status is-login
-        eval (dircolors -c ${ls-colors}/share/LS_COLORS/LS_COLORS | sed 's|setenv|set -Ux|')
-      end
-      set -g __fish_config_ls_colors_sourced 1
-    '';
-    nix = writeText "${name}-nix" ''
-      set -q __fish_config_nix_sourced
-      or begin
-        set NIX_PATH "nixpkgs=$HOME/repos/nixpkgs:$NIX_PATH"
-        [ (uname) = Darwin ]
-        and set NIX_PATH "darwin=$HOME/repos/nix-darwin:$NIX_PATH"
-        if status is-interactive
-          abbr --add nb  "nix build"
-          abbr --add nba "nix build -f '<nixpkgs>'"
-          abbr --add ne  "nix-env"
-          abbr --add nei "nix-env -f '<nixpkgs>' -iA"
-          abbr --add neq "nix-env -f '<nixpkgs>' -qaP --description"
-          abbr --add nel "nix-env --list-generations"
-          # abbr --add ned "nix-env --delete-generations"
-          # abbr --add ner "nix-env --rollback"
-          abbr --add nc  "nix-channel"
-          abbr --add ncu "nix-channel --update"
-          abbr --add nsh "nix-shell"
-          abbr --add ns  "nix-store"
-          abbr --add nsg "nix-store --gc"
-          abbr --add nr  "nix repl '<nixpkgs>'"
+    cfg = {
+      direnv = writeText "${name}-direnv" ''
+        set -q __fish_config_direnv_sourced
+        or eval (${direnv}/bin/direnv hook fish)
+        set -g __fish_config_direnv_sourced 1
+      '';
+      git = writeText "${name}-git" ''
+        set -q __fish_config_git_sourced
+        or if status is-interactive
+          set __fish_git_prompt_showdirtystate y
+          set __fish_git_prompt_showcolorhints y
+          set __fish_git_prompt_showstashstate y
+          set __fish_git_prompt_showuntrackedfiles y
+          set __fish_git_prompt_showupstream auto
+          set __fish_git_prompt_char_untrackedfiles '?'
+          set __fish_git_prompt_char_upstream_equal '''
+          set __fish_git_prompt_char_upstream_behind '↓'
+          set __fish_git_prompt_char_upstream_ahead '↑'
+          set __fish_git_prompt_char_upstream_diverged '↓↑'
+          set __fish_git_prompt_color_untrackedfiles $fish_status_color_warn
+          abbr --add g   'git'
+          abbr --add ga  'git add'
+          abbr --add gb  'git branch'
+          abbr --add gc  'git commit'
+          abbr --add gcl 'git clone'
+          abbr --add gco 'git checkout'
+          abbr --add gd  'git diff'
+          abbr --add gdc 'git diff --cached'
+          abbr --add gdd 'git diff --no-ext-diff'
+          abbr --add gl  'git log'
+          abbr --add glg 'git log --graph --oneline'
+          abbr --add gp  'git pull'
+          abbr --add gs  'git status'
+          abbr --add gss 'git show --ext-diff'
         end
-      end
-      set -g __fish_config_nix_sourced 1
-    '';
+        set -g __fish_config_git_sourced 1
+      '';
+      less = writeText "${name}-less" ''
+        set -q __fish_config_less_sourced
+        or if status is-login
+          # Set to fit Srcery color scheme
+          set -Ux LESS_TERMCAP_mb (printf "\e[1m")                    # Begin blinking
+          set -Ux LESS_TERMCAP_md (printf "\e[1;31m")                 # Begin bold
+          set -Ux LESS_TERMCAP_me (printf "\e[0m")                    # End mode
+          set -Ux LESS_TERMCAP_so (printf "\e[1;30;48;2;214;93;14m")  # Begin standout mode
+          set -Ux LESS_TERMCAP_se (printf "\e[0m")                    # End standout mode
+          set -Ux LESS_TERMCAP_us (printf "\e[3;33m")                 # Begin underline
+          set -Ux LESS_TERMCAP_ue (printf "\e[0m")                    # End underline
+        end
+        if status is-interactive
+          set -gx PAGER less
+          set -gx LESS '-R -ig -j.5'
+        end
+        set -g __fish_config_less_sourced 1
+      '';
+      lsColors = writeText "${name}-ls-colors" ''
+        set -q __fish_config_ls_colors_sourced
+        or if status is-login
+          eval (dircolors -c ${ls-colors}/share/LS_COLORS/LS_COLORS | sed 's|setenv|set -Ux|')
+        end
+        set -g __fish_config_ls_colors_sourced 1
+      '';
+      nix = writeText "${name}-nix" ''
+        set -q __fish_config_nix_sourced
+        or begin
+          set NIX_PATH "nixpkgs=$HOME/repos/nixpkgs:$NIX_PATH"
+          [ (uname) = Darwin ]
+          and set NIX_PATH "darwin=$HOME/repos/nix-darwin:$NIX_PATH"
+          if status is-interactive
+            abbr --add nb  "nix build"
+            abbr --add nba "nix build -f '<nixpkgs>'"
+            abbr --add ne  "nix-env"
+            abbr --add nei "nix-env -f '<nixpkgs>' -iA"
+            abbr --add neq "nix-env -f '<nixpkgs>' -qaP --description"
+            abbr --add nel "nix-env --list-generations"
+            # abbr --add ned "nix-env --delete-generations"
+            # abbr --add ner "nix-env --rollback"
+            abbr --add nc  "nix-channel"
+            abbr --add ncu "nix-channel --update"
+            abbr --add nsh "nix-shell"
+            abbr --add ns  "nix-store"
+            abbr --add nsg "nix-store --gc"
+            abbr --add nr  "nix repl '<nixpkgs>'"
+          end
+        end
+        set -g __fish_config_nix_sourced 1
+      '';
+    };
   in runCommand name {} ''
     confd="$out/etc/fish/conf.d"
-    install -D -m 444 ${git} "$confd"/git.fish
-    install -D -m 444 ${less} "$confd"/less.fish
-    install -D -m 444 ${lsColors} "$confd"/ls-colors.fish
-    install -D -m 444 ${nix} "$confd"/nix.fish
+    install -D -m 444 ${cfg.direnv} "$confd"/direnv.fish
+    install -D -m 444 ${cfg.git} "$confd"/git.fish
+    install -D -m 444 ${cfg.less} "$confd"/less.fish
+    install -D -m 444 ${cfg.lsColors} "$confd"/ls-colors.fish
+    install -D -m 444 ${cfg.nix} "$confd"/nix.fish
   '';
 
   jupyter = super.callPackage ../pkgs/jupyter {
