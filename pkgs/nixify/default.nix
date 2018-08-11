afcb620c4686c11addcb6d0b79df8dea31e9e76a
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-08-12 00:19:28 +0900

    Use substituteAll to generate nixify script

--- pkgs/nixify/default.nix
+++ pkgs/nixify/default.nix
@@ -1,63 +1,10 @@
-{ stdenv, writeText, runCommand, fish, vim }:
+{ stdenv, substituteAll, runCommand, fish, vim }:
 
 let
-  nixify = writeText "nixify" ''
-    #!${fish}/bin/fish
-
-    if [ ! -e ./.envrc ]
-      echo "use nix" > .envrc
-      direnv allow
-    end
-
-    if [ ! -e default.nix ]
-      echo -n "\
-    { nixpkgs ? import <nixpkgs> {} }:
-
-    with nixpkgs;
-
-    stdenv.mkDerivation rec {
-      name = \"my-pkg-\''${version}\";
-      version = \"0.0.1\";
-
-      src = ./.;
-
-      nativeBuildInputs = [ ];
-      buildInputs = [ ];
-    }
-    " > default.nix
-    end
-
-    if [ ! -e shell.nix ]
-      echo -n "\
-    with import <nixpkgs> {};
-
-    mkShell {
-      inputsFrom = [ (import ./default.nix {}) ];
-      buildInputs = [ ];
-
-      shellHook = '''
-      ''';
-    }
-    " > shell.nix
-    end
-
-    set -l ignore "\
-    # Nix direnv related stuff
-    .direnv
-    result
-    "
-    if [ ! -e ./.gitignore ]
-      echo -n "$ignore" > .gitignore
-    else
-      echo -n "$ignore" >> .gitignore
-    end
-
-    if [ -z "$EDITOR" ]
-      ${vim}/bin/vim default.nix
-    else
-      eval $EDITOR default.nix
-    end
-    '';
+  nixify = substituteAll {
+    src = ./nixify.fish;
+    inherit fish vim;
+  };
 in
 
 runCommand "nixify" {} ''
---
+++ pkgs/nixify/nixify.fish
@@ -0,0 +1,55 @@
+#!@fish@/bin/fish
+
+if [ ! -e ./.envrc ]
+  echo "use nix" > .envrc
+  direnv allow
+end
+
+if [ ! -e default.nix ]
+  echo -n "\
+{ nixpkgs ? import <nixpkgs> {} }:
+
+with nixpkgs;
+
+stdenv.mkDerivation rec {
+  name = \"my-pkg-\${version}\";
+  version = \"0.0.1\";
+
+  src = ./.;
+
+  nativeBuildInputs = [ ];
+  buildInputs = [ ];
+}
+" > default.nix
+end
+
+if [ ! -e shell.nix ]
+  echo -n "\
+with import <nixpkgs> {};
+
+mkShell {
+  inputsFrom = [ (import ./default.nix {}) ];
+  buildInputs = [ ];
+
+  shellHook = ''
+  '';
+}
+" > shell.nix
+end
+
+set -l ignore "\
+# Nix direnv related stuff
+.direnv
+result
+"
+if [ ! -e ./.gitignore ]
+  echo -n "$ignore" > .gitignore
+else
+  echo -n "$ignore" >> .gitignore
+end
+
+if [ -z "$EDITOR" ]
+  @vim@/bin/vim default.nix
+else
+  eval $EDITOR default.nix
+end
