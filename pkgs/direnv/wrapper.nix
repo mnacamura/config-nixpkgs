146514f373ab451dfd0c362c242adce343e68a01
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-08-17 23:20:27 +0900

    direnvWrapper: Fish hook calls wrapped direnv

--- pkgs/direnv/wrapper.nix
+++ pkgs/direnv/wrapper.nix
@@ -1,22 +1,32 @@
-{ direnv, runCommand, buildEnv, makeWrapper }:
+{ direnv, runCommand, writeFishVendorConfig, buildEnv, makeWrapper }:
 
 let
   configHome = runCommand "direnv-config-home" {} ''
     install -D -m 444 "${./direnvrc}" "$out/direnv/direnvrc"
   '';
+
+  hook = writeFishVendorConfig "direnv" ''
+    eval (${wrapper}/bin/direnv hook fish)
+  '';
+
+  wrapper = buildEnv {
+    name = "${direnv.name}-wrapper-without-fish-hook";
+
+    paths = [ direnv ];
+
+    # Hide ${direnv}/share/fish/vendor_conf.d/direnv.fish
+    pathsToLink = [ "/share/man" ];
+
+    buildInputs = [ makeWrapper ];
+
+    postBuild = ''
+      makeWrapper ${direnv}/bin/direnv $out/bin/direnv \
+        --set XDG_CONFIG_HOME "${configHome}"
+    '';
+  };
 in
 
 buildEnv {
   name = "${direnv.name}-wrapper";
-
-  paths = [ direnv ];
-
-  pathsToLink = [ "/share" ];
-
-  buildInputs = [ makeWrapper ];
-
-  postBuild = ''
-    makeWrapper ${direnv}/bin/direnv $out/bin/direnv \
-      --set XDG_CONFIG_HOME "${configHome}"
-  '';
+  paths = [ wrapper hook ];
 }
