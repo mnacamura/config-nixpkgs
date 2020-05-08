483dc2bd611560859d25fb145c8145df6e0a40b9
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-05-08 16:50:22 +0900

    nixify: 'install' installs files with mode 755 by default

--- pkgs/nixify/default.nix
+++ pkgs/nixify/default.nix
@@ -3,10 +3,11 @@
 let
   nixify = substituteAll {
     src = ./nixify.fish;
+
     inherit fish vim;
   };
 in
 
 runCommand "nixify" {} ''
-  install -D -m 555 ${nixify} $out/bin/nixify
+  install -D ${nixify} $out/bin/nixify
 ''
