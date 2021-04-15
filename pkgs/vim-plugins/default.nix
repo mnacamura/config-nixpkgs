0000cdcb59fa547a3f6538378afeb2df0fc94626
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-04-15 15:34:31 +0900

    vimPlugins.clever-f-vim: bump version

--- pkgs/vim-plugins/default.nix
+++ pkgs/vim-plugins/default.nix
@@ -7,12 +7,12 @@ in
 {
   clever-f-vim = buildVimPlugin {
     pname = "clever-f-vim";
-    version = "2020-04-15";
+    version = "2021-03-05";
     src = fetchFromGitHub {
       owner = "rhysd";
       repo = "clever-f.vim";
-      rev = "c9c444d36dc52b1989f769946435cf0541fab38b";
-      sha256 = "1fy4prd5y544jw4qk9v732jwy4yj52632iq3nxl587cmwjlijxpa";
+      rev = "3e79731f81ce7fba641c5dd5803f0978238d6204";
+      sha256 = "0py012wq78ikrxm1wsdk4p052l2bwz0hhnwig0601smcbni5ak86";
     };
   };
 
