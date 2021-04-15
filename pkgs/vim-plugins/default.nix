26526dee7e097d553b390c49f69151ff19cb9c11
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-04-15 15:50:15 +0900

    vimPlugins.vim-yoink: bump version

--- pkgs/vim-plugins/default.nix
+++ pkgs/vim-plugins/default.nix
@@ -31,12 +31,12 @@ in
 
   vim-yoink = buildVimPlugin {
     pname = "vim-yoink";
-    version = "2020-05-23";
+    version = "2020-10-14";
     src = fetchFromGitHub {
       owner = "svermeulen";
       repo = "vim-yoink";
-      rev = "17b349d49156887d9203e3fac6fa072746f505c0";
-      sha256 = "1whmgq368f7ppsmncz4nr9qc5wjjw5kjxizsadjm2zii1b3pifdc";
+      rev = "b973fce71d45fe7c290119448651da7a1b9943a1";
+      sha256 = "1cjrv55x340i5c87pd8z3l27f2jl98p0v20hr77is8f9fi3ja40d";
     };
   };
 
