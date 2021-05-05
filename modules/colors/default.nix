dfab1cf7b453e335288b1bf91426dd336b7ecde3
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-05-05 12:22:04 +0900

    module/colors: Sync with mnacamura/etc-nixos

--- modules/colors/default.nix
+++ modules/colors/default.nix
@@ -9,23 +9,8 @@ let
 
   colorHex = with types; strMatching "^#[0-9abcdefABCDEF]{6}$";
 
-  defaultPalette = {
-    black     = { nr =  0; hex = "#000000"; };
-    red       = { nr =  1; hex = "#800000"; };
-    green     = { nr =  2; hex = "#008000"; };
-    yellow    = { nr =  3; hex = "#808000"; };
-    blue      = { nr =  4; hex = "#000080"; };
-    magenta   = { nr =  5; hex = "#800080"; };
-    cyan      = { nr =  6; hex = "#008080"; };
-    white     = { nr =  7; hex = "#c0c0c0"; };
-    brblack   = { nr =  8; hex = "#808080"; };
-    brred     = { nr =  9; hex = "#ff0000"; };
-    brgreen   = { nr = 10; hex = "#00ff00"; };
-    bryellow  = { nr = 11; hex = "#ffff00"; };
-    brblue    = { nr = 12; hex = "#0000ff"; };
-    brmagenta = { nr = 13; hex = "#ff00ff"; };
-    brcyan    = { nr = 14; hex = "#00ffff"; };
-    brwhite   = { nr = 15; hex = "#ffffff"; };
+  palettes = {
+    srcery = import ./palettes/srcery.nix;
   };
 in
 
@@ -36,15 +21,31 @@ in
         color256
         colorHex
       ]));
-      default = defaultPalette;
+      default = palettes.srcery;
       description = ''
         Color theme to be used for various packages (console, vim, ...). Each
-        color is defined by an attrset which have at least 'nr' and 'hex' attrs:
+        color is defined by an attrset which have 'nr' and 'hex' attrs:
         'nr' is an xterm color number (e.g., black is 0), and 'hex' is a hex
-        color code with prefix '#' (e.g., "#1c1c1a"). It should include at
-        least 16 colors: 'black', 'red', 'green', 'yellow', 'blue', 'magenta',
-        'cyan', 'white', 'brblack', 'brred', 'brgreen', 'bryellow', 'brblue',
-        'brmagenta', 'brcyan', and 'brwhite'.
+        color code with prefix '#' (e.g., "#1c1c1a"). It should include colors
+        for terminal use, 'term_fg', 'term_bg', 'black', 'red', 'green',
+        'yellow', 'blue', 'magenta', 'cyan', 'white', 'brblack', 'brred',
+        'brgreen', 'bryellow', 'brblue', 'brmagenta', 'brcyan', 'brwhite',
+        and colors for GUI use, 'bg', 'fg', 'menu_fg', 'menu_bg', 'hdr_fg',
+        'hdr_bg', 'sel_bg', 'sel_fg', 'accent', 'txt_bg', 'txt_fg', 'btn_bg',
+        'btn_fg', 'hdr_btn_bg', 'hdr_btn_fg', 'wm_border_focus',
+        'wm_border_unfocus', 'caret1_fg', 'caret2_fg', 'icons_light',
+        'icons_medium', 'icons_dark', 'icons_sym_action', and 'icons_sym_panel'.
+        For more information about these colors, try to use the Themix GUI
+        designer.
+      '';
+    };
+
+    environment.colors.palettes = mkOption {
+      type = with types; attrsOf (attrsOf attrs);
+      default = palettes;
+      description = ''
+        A collection of color themes. For more information, see
+        'environment.colors.palette'.
       '';
     };
 
